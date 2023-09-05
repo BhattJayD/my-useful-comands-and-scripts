43a2d14a913d5296d9b5f2d00be444b2e33384a3
bhattjayd <cbhatt526@gmail.com>
2023-09-05 16:56:28 +0530

    take input from user if user want something like zsh

--- bashAlias.sh
+++ bashAlias.sh
@@ -1,30 +1,61 @@
 #!/bin/bash
-echo " \n \n \n \n " >> ~/.bashrc
-echo "########################## ANDROID ######################" >> ~/.bashrc
 
-echo "export ANDROID_HOME=$HOME/Android/Sdk" >> ~/.bashrc
-echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> ~/.bashrc
-echo "export PATH=$PATH:$ANDROID_HOME/platform-tools" >> ~/.bashrc
+if [ $# -eq 1 ]; then
 
-echo "#########################################################" >> ~/.bashrc
-echo " \n \n \n \n " >> ~/.bashrc
+    echo " \n \n \n \n " >> $1
+    echo "########################## ANDROID ######################" >> $1
+
+    echo "export ANDROID_HOME=$HOME/Android/Sdk" >> $1
+    echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> $1
+    echo "export PATH=$PATH:$ANDROID_HOME/platform-tools" >> $1
+
+    echo "#########################################################" >> $1
+    echo " \n \n \n \n " >> $1
 
 
-#echo "alias ip="ifconfig wlp3s0 | grep inet|head -n 1 | awk '{printf \$2}';echo "" >> ~/.bashrc
-#echo "alias ip="hostname -I | awk '{printf \$1}'"" >> ~/.bashrc
-echo "alias l='ls -lrtha --color=auto'" >> ~/.bashrc
-echo "alias i='sudo apt install'" >> ~/.bashrc
-echo "alias u='sudo apt update && sudo apt upgrade -y'" >> ~/.bashrc
-echo "alias thm='cd ~/Desktop/ctf/thm'" >> ~/.bashrc
-echo "alias htb='cd ~/Desktop/ctf/htb'" >> ~/.bashrc
-echo "alias live='cd ~/Desktop/ctf/ctfTime'" >> ~/.bashrc
-echo "alias up='python3 -m http.server'" >> ~/.bashrc  
-echo "alias pi='curl https://ifconfig.me '" >> ~/.bashrc
-echo "alias m='function _m() { mkdir "$1" && cd "$1"; }; _m'" >> ~/.bashrc
-echo "alias ovpn='sudo openvpn ~/vpn/SplitUnknown.ovpn'" >> ~/.bashrc
-echo "alias pc='function pc() { ping "$1" "$2"; }; pc'" >> ~/.bashrc
-echo "alias clone='function clone() { git clone "$1"; }; clone'" >> ~/.bashrc
+    #echo "alias ip="ifconfig wlp3s0 | grep inet|head -n 1 | awk '{printf \$2}';echo "" >> $1
+    #echo "alias ip="hostname -I | awk '{printf \$1}'"" >> $1
+    echo "alias l='ls -lrtha --color=auto'" >> $1
+    echo "alias i='sudo apt install'" >> $1
+    echo "alias u='sudo apt update && sudo apt upgrade -y'" >> $1
+    echo "alias thm='cd ~/Desktop/ctf/thm'" >> $1
+    echo "alias htb='cd ~/Desktop/ctf/htb'" >> $1
+    echo "alias live='cd ~/Desktop/ctf/ctfTime'" >> $1
+    echo "alias up='python3 -m http.server'" >> $1  
+    echo "alias pi='curl https://ifconfig.me '" >> $1
+    echo "alias m='function _m() { mkdir "$1" && cd "$1"; }; _m'" >> $1
+    echo "alias ovpn='sudo openvpn ~/vpn/SplitUnknown.ovpn'" >> $1
+    echo "alias pc='function pc() { ping "$1" "$2"; }; pc'" >> $1
+    echo "alias clone='function clone() { git clone "$1"; }; clone'" >> $1
 
+else
+
+    echo " \n \n \n \n " >> ~/.bashrc
+    echo "########################## ANDROID ######################" >> ~/.bashrc
+
+    echo "export ANDROID_HOME=$HOME/Android/Sdk" >> ~/.bashrc
+    echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> ~/.bashrc
+    echo "export PATH=$PATH:$ANDROID_HOME/platform-tools" >> ~/.bashrc
+
+    echo "#########################################################" >> ~/.bashrc
+    echo " \n \n \n \n " >> ~/.bashrc
+
+
+    #echo "alias ip="ifconfig wlp3s0 | grep inet|head -n 1 | awk '{printf \$2}';echo "" >> ~/.bashrc
+    #echo "alias ip="hostname -I | awk '{printf \$1}'"" >> ~/.bashrc
+    echo "alias l='ls -lrtha --color=auto'" >> ~/.bashrc
+    echo "alias i='sudo apt install'" >> ~/.bashrc
+    echo "alias u='sudo apt update && sudo apt upgrade -y'" >> ~/.bashrc
+    echo "alias thm='cd ~/Desktop/ctf/thm'" >> ~/.bashrc
+    echo "alias htb='cd ~/Desktop/ctf/htb'" >> ~/.bashrc
+    echo "alias live='cd ~/Desktop/ctf/ctfTime'" >> ~/.bashrc
+    echo "alias up='python3 -m http.server'" >> ~/.bashrc  
+    echo "alias pi='curl https://ifconfig.me '" >> ~/.bashrc
+    echo "alias m='function _m() { mkdir "$1" && cd "$1"; }; _m'" >> ~/.bashrc
+    echo "alias ovpn='sudo openvpn ~/vpn/SplitUnknown.ovpn'" >> ~/.bashrc
+    echo "alias pc='function pc() { ping "$1" "$2"; }; pc'" >> ~/.bashrc
+    echo "alias clone='function clone() { git clone "$1"; }; clone'" >> ~/.bashrc
+fi
 
 
 # Installing SecList
